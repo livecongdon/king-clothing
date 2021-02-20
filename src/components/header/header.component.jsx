a7f6b6da90ec23dc869dd22be5623facd9dcfb68
Mike <livecongdon@gmail.com>
2021-02-20 16:05:27 -0600

    implemented firebase utils and store authenticated user in firestore

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { auth } from '../../firebase/firebase.utils';
 
-const Header = () => (
+const Header = ({ currentUser }) => (
     <div className='header'>
         <Link to='/'>
             <Logo className='logo-container' to='/' />
@@ -18,9 +18,9 @@ const Header = () => (
             </Link>
             {
                 currentUser ?
-                <div className='option' onClick={() => auth.signOut}>SIGN OUT</div>
+                <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
                 :
-                <link className='option' to='/signin'>SIGN IN</link>
+                <Link className='option' to='/signin'>SIGN IN</Link>
             }
         </div>
     </div>
